0cc0957634751e0d66b8a65940303e2b8e917322
notus1usg <notus1usg@gmail.com>
2014-02-16 19:29:45 -0800

    test

---
+++ test_git.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by SonHA
+ * User: Son Ha Anh (sonhaanh@vccorp.vn)
+ * Date: 2/17/14
+ * Time: 10:28 AM
+ * To change this template use File | Settings | File Templates.
+ */
+echo 'test git';
