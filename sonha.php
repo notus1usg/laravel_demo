79ddb8514893dd8f2ab430639b9c08386adaf2ea
notus1usg <notus1usg@gmail.com>
2014-02-17 20:20:44 -0800

    test

---
+++ sonha.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by SonHA
+ * User: Son Ha Anh (sonhaanh@vccorp.vn)
+ * Date: 2/18/14
+ * Time: 11:18 AM
+ * To change this template use File | Settings | File Templates.
+ */
+echo 'sonhaanh';
